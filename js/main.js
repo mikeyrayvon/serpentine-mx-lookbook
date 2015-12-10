95ad0a1acd36c2b8d30e05e78ee6efa69005933d
mikey <michaelrayvon@gmail.com>
2015-12-09 18:37:29 -0600

    add turn js functions and sizing functions

--- js/main.js
+++ js/main.js
@@ -1,31 +1,39 @@
 /* jshint browser: true, devel: true, indent: 2, curly: true, eqeqeq: true, futurehostile: true, latedef: true, undef: true, unused: true */
 /* global $, jQuery, document, Modernizr */
 
-function l(data) {
-  'use strict';
-  console.log(data);
-}
-
 $(document).ready(function () {
   'use strict';
 
-});
+  var lookbook = $('#lookbook'),
+    winHeight = $(window).height(),
+    winWidth = $(window).width(),
+    header = $('#header'),
+    footer = $('#footer'),
+    headerHeight = header.height(),
+    footerHeight = footer.height(),
+    lookbookHeight = ( winHeight - ( ( headerHeight + footerHeight ) * 2 ) ),
+    lookbookWidth = ( ( lookbookHeight * 0.707 ) * 2 );
 
-$(window).load(function () {
-
-  /* PACKERY */
-  var pckry = new Packery( document.getElementById("gallery-container"), {
-    // options
-    itemSelector: '.photo',
-    percentPosition: true,
-    columnWidth: '.photo',
-    gutter: 0
+  lookbook.css({
+    'margin-top': ( ( winHeight - lookbookHeight ) / 2 ),
+    'margin-left': ( ( winWidth - lookbookWidth ) / 2 ),
+  }).turn({
+    height: lookbookHeight,
+    width: lookbookWidth,
+    autoCenter: true,
   });
 
-  // Display color overlay
-  $('.photo').addClass('color-overlay');
 
-  $('#gallery').animate({
-    opacity: 1
-  }, 600);
+  $(window).on('resize', function(){
+    winHeight = $(window).height();
+    winWidth = $(window).width();
+    lookbookHeight = ( winHeight - ( ( headerHeight + footerHeight ) * 2 ) ),
+    lookbookWidth = ( ( lookbookHeight * 0.707 ) * 2 );
+
+    lookbook.css({
+      'margin-top': ( ( winHeight - lookbookHeight ) / 2 ),
+      'margin-left': ( ( winWidth - lookbookWidth ) / 2 ),
+    }).turn('size', lookbookWidth, lookbookHeight);
+  });
+
 });
